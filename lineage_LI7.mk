a9fda0096221990759be12ec50cfae9964e0c4bd
HAPPYGAMES222 <115911805+HAPPYGAMES222@users.noreply.github.com>
2025-01-18 21:53:41 +0700

    LI7: WELCOME TO LI7

--- lineage_LI7.mk
+++ lineage_LI7.mk
@@ -11,23 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from X6837 device
-$(call inherit-product, device/infinix/X6837/device.mk)
+# Inherit from LI7 device
+$(call inherit-product, device/tecno/LI7/device.mk)
 
-BOARD_VENDOR := Infinix
-PRODUCT_NAME := lineage_X6837
-PRODUCT_DEVICE := X6837
-PRODUCT_MANUFACTURER := INFINIX
-PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix X6837
+BOARD_VENDOR := Tecno
+PRODUCT_NAME := lineage_LI7
+PRODUCT_DEVICE := LI7
+PRODUCT_MANUFACTURER := TECNO
+PRODUCT_BRAND := Tecno
+PRODUCT_MODEL := Tecno LI7
 
 PRODUCT_GMS_CLIENTID_BASE := android-transsion
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sys_tssi_64_armv82_infinix-user 13 TP1A.220624.014 526124 release-keys" \
-    PRODUCT_NAME=X6837-OP
+    PRIVATE_BUILD_DESC="sys_tssi_64_armv82_tecno_dolby-user 14 UP1A.231005.007 632491 release-keys" \
+    PRODUCT_NAME=LI7-OP
 
-BUILD_FINGERPRINT := Infinix/X6837-OP/Infinix-X6837:13/TP1A.220624.014/240929V1624:user/release-keys
+BUILD_FINGERPRINT := TECNO/LI7-OP/TECNO-LI7:14/UP1A.231005.007/240610V967:user/release-keys
 
 # Time
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
